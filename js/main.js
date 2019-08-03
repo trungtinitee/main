8b658f7c19cddcb495c2a87498b26f930a09fae4
Lu Trung Tin <49675459+trungtinitee@users.noreply.github.com>
2019-08-03 13:52:22 +0700

    03

--- js/main.js
+++ js/main.js
@@ -1,19 +1,62 @@
 // *********************** THIS IS JAVA SCRIPT FILE *************************
 //*************************************************************************** */
 
+//VAR
+
+
+
 // FUNCTION
 
-function into_logo(){
-    document.getElementById("logo1").style.display = "none";
-    document.getElementById("logo2").style.display = "block";
-}
-function leave_logo(){
-    document.getElementById("logo1").style.display = "block";
-    document.getElementById("logo2").style.display = "none";
+// change text for introduce
+
+function change_text_for_introduce(){
+    var temp_wait = 0;
+    var repeart_change_text = setInterval(function_repeart_text,10000);
+    function function_repeart_text(){
+    if(temp_wait == 4){
+        temp_wait = 0;
+    }
+    
+    if (temp_wait == 0){
+        document.getElementById("intro1").style.display = "none";
+        document.getElementById("intro2").style.display = "block";
+        document.getElementById("intro3").style.display = "none";
+        document.getElementById("intro4").style.display = "none";
+
+    }
+
+    if (temp_wait == 1){
+        document.getElementById("intro1").style.display = "none";
+        document.getElementById("intro3").style.display = "block";
+        document.getElementById("intro2").style.display = "none";
+        document.getElementById("intro4").style.display = "none";
+
+    }
+
+    if (temp_wait == 2){
+        document.getElementById("intro1").style.display = "none";
+        document.getElementById("intro4").style.display = "block";
+        document.getElementById("intro2").style.display = "none";
+        document.getElementById("intro3").style.display = "none";
+
+    }
+
+    if (temp_wait == 3){
+        document.getElementById("intro4").style.display = "none";
+        document.getElementById("intro1").style.display = "block";
+        document.getElementById("intro2").style.display = "none";
+        document.getElementById("intro3").style.display = "none";
+
+    }
+
+    temp_wait +=1;
+    }
 }
 
 
 // SCRIPT FOR LOGO HEADER
 
-document.getElementById("logo1").addEventListener("mouseover", into_logo);
-document.getElementById("logo2").addEventListener("mouseleave", leave_logo);
+
+
+//when form load
+change_text_for_introduce();
