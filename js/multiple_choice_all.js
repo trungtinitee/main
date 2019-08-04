fb3453c5b493971b070c1831010dcebbf6fed7bf
Lu Trung Tin <49675459+trungtinitee@users.noreply.github.com>
2019-08-04 14:39:16 +0700

    a

---
+++ js/multiple_choice_all.js
@@ -0,0 +1,36 @@
+// *********************** THIS IS JAVA SCRIPT FILE *************************
+//*************************************************************************** */
+
+//VAR
+
+
+
+// FUNCTION
+
+// change text for introduce
+
+// SCRIPT FOR HEADER
+var status_header = false;
+function header_page(){
+    if (status_header == false){
+        document.getElementById("header_page").style.display = "block";
+        document.getElementById("line_header").style.display = "none";
+        document.getElementById("header_reponsive").style.height = "0";
+        document.getElementById("btn_expand").style.top = "160px";
+        status_header = true;
+    }
+    else{
+        document.getElementById("header_page").style.display = "none";
+        document.getElementById("btn_expand").style.top = "20px";
+        document.getElementById("line_header").style.display = "block";
+        document.getElementById("header_reponsive").style.height = "50px";
+        status_header = false;
+    }
+    
+    
+
+}
+
+
+//CODE WHEN FORM LOAD
+document.getElementById("btn_expand").addEventListener("click",header_page);
